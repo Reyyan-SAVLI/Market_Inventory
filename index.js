666a4535962b801335b9807a58557fa1d7cc8a0e
Reyyan Savli <reyyansvli@gmail.com>
2024-02-20 17:41:30 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const db = require('./models/conn');
+const bodyParser = require('body-parser');
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, 'pages')));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/', (req, res)=>{
+  const filePath = path.join(__dirname, 'pages/login.html');
+  res.sendFile(filePath, (err)=>{
+    if (err) {
+        console.log(err);
+    }
+  });
+});
+
+app.post('/', (req, res)=>{
+   const email = req.body.uname;
+   const password = req.body.psw;
+
+   const sql = 'SELECT * FROM users WHERE email= ? AND password= ?';
+
+   db.query(sql, [email, password], (err, result)=>{
+    if(err) throw err;
+
+    if (result.length > 0) {
+        res.redirect('/main');
+      } else {
+        res.send('Kullanıcı Adı veya Şifre Hatalı');
+      }
+   });
+});
+
+app.get('/main', (req, res)=>{
+   const filePath = path.join(__dirname, 'pages/main.html');
+   res.sendFile(filePath, (err)=>{
+    if(err){
+      console.log(err);
+    }
+   });
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
