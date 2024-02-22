31aa3cef3c9aa66904db23471b579b4a9013a510
Reyyan Savli <reyyansvli@gmail.com>
2024-02-22 17:37:02 +0300

    m

--- index.js
+++ index.js
@@ -44,18 +44,89 @@ app.get('/main', (req, res)=>{
    });
 });
 
-app.post('/main', (req, res)=>{
-  
-});
+app.get('/main/data', (req, res)=>{
+  const sql = 'SELECT * FROM products';
+   db.query(sql, (err, result)=>{
+     if(err) throw err;
 
-app.get('/products', (req, res)=>{
-  const filePath = path.join(__dirname, 'pages/main.html');
+     res.json(result);
+   })
+  });
+
+app.get('/markets', (req, res)=>{
+  const filePath = path.join(__dirname, 'pages/markets.html');
   res.sendFile(filePath, (err)=>{
    if(err){
      console.log(err);
    }
   });
 });
+
+app.get('/markets/data', (req, res)=>{
+  const sql = 'SELECT * FROM markets';
+   db.query(sql, (err, result)=>{
+     if(err) throw err;
+
+     res.json(result);
+   })
+  });
+
+app.get('/storages', (req, res)=>{
+  const filePath = path.join(__dirname, 'pages/storages.html');
+  res.sendFile(filePath, (err)=>{
+   if(err){
+     console.log(err);
+   }
+  });
+});
+
+app.get('/storages/data', (req, res)=>{
+  const sql = 'SELECT * FROM storages';
+   db.query(sql, (err, result)=>{
+     if(err) throw err;
+
+     res.json(result);
+   })
+  });
+
+app.get('/stock', (req, res)=>{
+  const filePath = path.join(__dirname, 'pages/stock.html');
+  res.sendFile(filePath, (err)=>{
+   if(err){
+     console.log(err);
+   }
+  });
+});
+
+app.get('/stock/data', (req, res)=>{
+  const sql = 'SELECT s.name, s.storagecode, s.city, m.name, m.marketcode, m.city, p.name, p.amount FROM marketinventory.storages s INNER JOIN marketinventory.markets m ON s.marketid = m.id INNER JOIN marketinventory.products p ON s.productid = p.id ';
+   db.query(sql, (err, result)=>{
+     if(err) throw err;
+
+    
+    res.json(result);
+
+   })
+  });
+
+app.get('/profile', (req, res)=>{
+  const filePath = path.join(__dirname, 'pages/profile.html');
+  res.sendFile(filePath, (err)=>{
+   if(err){
+     console.log(err);
+   }
+  });
+});
+
+app.get('/profile/data', (req, res)=>{
+  const sql = 'SELECT * FROM users';
+   db.query(sql, (err, result)=>{
+     if(err) throw err;
+
+     console.log(result);
+     res.json(result);
+   })
+  });
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
