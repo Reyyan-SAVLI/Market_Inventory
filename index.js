e6c1bddd08a31811ab49e7855c76f045befde401
Reyyan Savli <reyyansvli@gmail.com>
2024-02-26 17:54:12 +0300

    26.02

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ const app = express();
 const db = require('./models/conn');
 const bodyParser = require('body-parser');
 const fileUpload = require('express-fileupload');
-const { name } = require('ejs');
-const { error } = require('console');
 const port = 3000;
 
 app.use(express.static(path.join(__dirname, 'pages')));
@@ -67,11 +65,15 @@ app.get('/markets', (req, res)=>{
 });
 
 app.get('/markets/data', (req, res)=>{
-  const sql = 'SELECT * FROM markets';
+  const sql = 'SELECT * FROM markets m INNER JOIN products p ON m.productid = p.id';
    db.query(sql, (err, result)=>{
-     if(err) throw err;
-
-     res.json(result);
+     if(err){
+      throw err;
+     }else{
+      const itemsBelow100 = result.filter(item => item.amount < 100);
+            res.json({ marketData: result, itemsBelow100 });
+     }
+     
    })
   });
 
@@ -85,7 +87,7 @@ app.get('/storages', (req, res)=>{
 });
 
 app.get('/storages/data', (req, res)=>{
-  const sql = 'SELECT * FROM storages';
+  const sql = 'SELECT * FROM storages s INNER JOIN products p ON s.productid = p.id';
    db.query(sql, (err, result)=>{
      if(err) throw err;
 
@@ -103,7 +105,7 @@ app.get('/stock', (req, res)=>{
 });
 
 app.get('/stock/data', (req, res)=>{
-  const sql = 'SELECT s.name, s.storagecode, s.city, m.name, m.marketcode, m.city, p.name, p.amount FROM marketinventory.storages s INNER JOIN marketinventory.markets m ON s.marketid = m.id INNER JOIN marketinventory.products p ON s.productid = p.id ';
+  const sql = 'SELECT * FROM storages s INNER JOIN markets m ON s.marketid = m.id INNER JOIN products p ON s.productid = p.id ';
    db.query(sql, (err, result)=>{
      if(err) throw err;
 
@@ -131,6 +133,21 @@ app.get('/profile/data', (req, res)=>{
    })
   });
 
+  app.post('/profile', (req, res)=>{
+   const sql = 'UPDATE users SET name = ?, surname = ?, email = ?, phone = ?, password = ? ';
+   const name = req.body.txtname;
+   const surname = req.body.txtsurname;
+   const email = req.body.txtemail;
+   const phone = req.body.txtphone;
+   const psw = req.body.txtpsw;
+   db.query(sql, [name, surname, email, phone, psw], (err, result)=>{
+     if(err) throw err;
+
+     res.redirect('/profile');
+     console.log('Bilgiler güncellendi.');
+   });
+  });
+
   app.get('/addproduct', (req, res)=>{
     const filePath = path.join(__dirname, 'pages/addproduct.html');
     res.sendFile(filePath, (err)=>{
@@ -153,7 +170,6 @@ app.get('/profile/data', (req, res)=>{
     const pImage = `/assets/${req.files.pImage.name}`;
     const sql = 'INSERT INTO products (name, imagepath, brand, barcode, price, kdvratio, productcode, amount, category, subcategory) VALUES (?,?,?,?,?,?,?,?,?,?)';
   
-    // Dikkat: Burada `!==` kullanılmalı, yanlış bir atama kontrolü yapılıyor.
     if (req.files.pImage.mimetype !== 'image/jpeg') {
       return res.status(400).send('Sadece JPG formatlı resimler kabul edilir.');
     }
